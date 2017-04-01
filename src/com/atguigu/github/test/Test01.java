ced52e78271db5cf9bc885369c8297c8eb4a62f4
xieyong236 <xieyong236@126.com>
2017-04-01 15:20:05 +0800

    Random Update
    Random Update

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -6,5 +6,6 @@ public class Test01
             {
                 System.out.println("hello,github server! push ...");
                 System.out.println("hello,github server! pull ...");
+                System.out.println("GitHub Server Info: Random Update ...");
             }
     }
