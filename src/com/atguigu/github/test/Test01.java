19b908c0db691a9e10fee720eebebfb7800563e9
xieyong236 <xieyong236@126.com>
2017-04-01 14:54:12 +0800

    test pull

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -4,6 +4,7 @@ public class Test01
     {
         public static void main(String[] args)
             {
-                System.out.println("hello,github server!");
+                System.out.println("hello,github server! push ...");
+                System.out.println("hello,github server! pull ...");
             }
     }
